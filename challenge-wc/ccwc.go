98452ee61d6ffa77f4f38b7550b9921c3245783f
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-04-19 19:27:46 +0530

    Update the line count

--- challenge-wc/ccwc.go
+++ challenge-wc/ccwc.go
@@ -3,16 +3,36 @@ package main
 import (
   "fmt"
   "os"
+  "bufio"
 )
 
 func main() {
   argsWithProg := os.Args
   args := argsWithProg[1:]
-  if args[0] == "-c" {
-    file, err := os.Stat(args[1])
+  file, err := os.Stat(args[1])
+  if err != nil {
+    fmt.Println("File does not exist")
+  }
+  if args[0] == "-c" { 
+        fmt.Println(file.Size(), args[1])
+  } else if (args[0] == "-l") {
+    file, err := os.Open(args[1])
     if err != nil {
-      fmt.Println("File does not exist")
-    }
-    fmt.Println(file.Size(), args[1])
+		fmt.Println("Error:", err)
+		return
+	}
+	defer file.Close()
+
+	lineCount := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lineCount++
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+     fmt.Println(lineCount, args[1])
   }
 }
