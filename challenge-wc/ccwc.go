0c7d968158c7aefffa047abc1d3090be29f5ee64
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-04-20 23:04:50 +0530

    Step 3 completed

--- challenge-wc/ccwc.go
+++ challenge-wc/ccwc.go
@@ -1,40 +1,50 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "bufio"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-  argsWithProg := os.Args
-  args := argsWithProg[1:]
-  file, err := os.Stat(args[1])
-  if err != nil {
-    fmt.Println("File does not exist")
-  }
-  if args[0] == "-c" { 
-        fmt.Println(file.Size(), args[1])
-  } else if (args[0] == "-l") {
-    file, err := os.Open(args[1])
-    if err != nil {
-		  fmt.Println("Error:", err)
-		  return
-	  }
-	  defer file.Close()
-
-	  lineCount := 0
-	  scanner := bufio.NewScanner(file)
-	  for scanner.Scan() {
-		  lineCount++
-	  }
-
-	  if err := scanner.Err(); err != nil {
+	argsWithProg := os.Args
+	args := argsWithProg[1:]
+	file, err := os.Open(args[1])
+	if err != nil {
 		fmt.Println("Error:", err)
 		return
-	  }
-    fmt.Println(lineCount, args[1])
-  } else if (args[0] == "-w") {
+	}
+	defer file.Close()
+	if args[0] == "-c" {
+		file, err := os.Stat(args[1])
+		if err != nil {
+			fmt.Println("File does not exist")
+		}
+		fmt.Println(file.Size(), args[1])
+	} else if args[0] == "-l" {
+		lineCount := 0
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			lineCount++
+		}
 
-  }
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		fmt.Println(lineCount, args[1])
+	} else if args[0] == "-w" {
+		wordCount := 0
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := strings.Fields(scanner.Text())
+			for _, w := range line {
+				if w != "" {
+					wordCount++
+				}
+			}
+		}
+		fmt.Println(wordCount, args[1])
+	}
 }
